2db59dd84c0a2499772163abff391e0d7415369d
TJ Holowaychuk <tj@vision-media.ca>
2009-01-03 20:31:33 -0800

    - Doc

--- lib/dm-forms/elements.rb
+++ lib/dm-forms/elements.rb
@@ -2,6 +2,18 @@
 module DataMapper
   module Forms
     module Elements
+      
+      ##
+      # Generates a generic HTML +name+ tag. Although this method is 
+      # generally used internally by dm-forms, you may utilize it directly
+      # passing any of the following +options+.
+      #
+      # Options:
+      #
+      #   :self_closing   Wither or not the element should self-close (<br />)
+      #   :attributes     Hash of attributes such as :type => :textfield
+      #
+      
       def tag name, options = {}
         self_closing = options[:self_closing]
         value = options[:attributes].delete(:value) if self_closing rescue ''
@@ -11,6 +23,13 @@ module DataMapper
           else
             " />#{value}</#{name}>"
           end
+      end
+      
+      ##
+      # Generates an HTML textfield.
+      
+      def textfield attributes = {}
+        
       end
     end
   end
--- spec/functional/elements_spec.rb
+++ spec/functional/elements_spec.rb
@@ -14,4 +14,9 @@ describe DataMapper::Forms::Elements do
     tag(:input, :attributes => { :type => :textfield }, :self_closing => true).should == '<input type="textfield" />'
   end
   
+  it "should create textfields" do
+    s = textfield :name => :phone, :value => 'Enter phone number'
+    s.should == '<intput type="textfield" value="Enter phone number" />'
+  end
+  
 end
