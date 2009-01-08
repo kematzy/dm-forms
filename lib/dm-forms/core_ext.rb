dbd410459ad424c5e978182313f4c92c4c5c7a9e
TJ Holowaychuk <tj@vision-media.ca>
2009-01-08 11:22:15 -0800

    - Added #humanize for syms and strings

--- lib/dm-forms/core_ext.rb
+++ lib/dm-forms/core_ext.rb
@@ -5,3 +5,17 @@ class NilClass
   end
   alias :to_html_attributes :to_xml_attributes
 end
+
+class String
+    
+  def humanize
+    gsub!(/[^a-zA-Z\d]/, ' ')
+  end
+end
+
+class Symbol
+  
+  def humanize
+    to_s.humanize
+  end
+end
--- spec/functional/core_ext_spec.rb
+++ spec/functional/core_ext_spec.rb
@@ -2,11 +2,11 @@
 describe "#humanize" do
   
   it "should humanize strings" do
-    
+    'im_a_Lame_String.Yeah.with.7.words'.humanize.should == 'im a Lame String Yeah with 7 words'
   end
   
   it "should humanize symbols" do
-    
+    :some_Cool_symbol.humanize.should == 'some Cool symbol'
   end
   
 end
