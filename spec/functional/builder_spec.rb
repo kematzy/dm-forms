ae8256fb1945842a6b999b491db231203a1a9488
TJ Holowaychuk <tj@vision-media.ca>
2009-02-24 15:59:28 -0800

    - #tag spec

---
+++ spec/functional/builder_spec.rb
@@ -0,0 +1,14 @@
+
+describe DataMapper::Form::Builder do
+  
+ describe "Base" do
+   before :each do
+     @builder = DataMapper::Form::Builder::Base.new
+   end
+   
+   it "should description" do
+
+   end
+ end
+  
+end
