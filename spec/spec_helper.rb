5d6709973b9be69e892810737d3e007f603229d1
TJ Holowaychuk <tj@vision-media.ca>
2009-02-24 15:47:37 -0800

    - Removed old specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,3 @@ require 'rspec_hpricot_matchers'
 Spec::Runner.configure do |config|
   config.include RspecHpricotMatchers
 end
-  
-class String
-  def dedent spaces = 6
-    gsub /^ {0,#{spaces}}/, ''
-  end
-end
