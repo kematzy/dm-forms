fd24f5356bebc8da1e440c7e8a6d430e2b2ece47
TJ Holowaychuk <tj@vision-media.ca>
2009-02-24 16:18:57 -0800

    - Refactoring some more

--- spec/functional/builder_spec.rb
+++ spec/functional/builder_spec.rb
@@ -1,15 +1,6 @@
 
 describe DataMapper::Form::Builder do
   
-  include DataMapper::Form::Helpers
-  
-  it "should description" do
-    results = form :action => '/login' do
-      text_field :name, :label => 'Username'
-      password_field :pass, :label => 'Password'
-      submit 'Login'
-    end
-    p results
-  end
+
   
 end
