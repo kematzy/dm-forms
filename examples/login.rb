2f82453c1b49599a5f82affa7e1a26b5f869ff67
TJ Holowaychuk <tj@vision-media.ca>
2009-02-25 20:25:22 -0800

    - Added some mixins to login example

--- examples/login.rb
+++ examples/login.rb
@@ -2,9 +2,14 @@
 require File.dirname(__FILE__) + '/../lib/dm-forms'
 
 include DataMapper::Form::Helpers
+DataMapper::Form::Base.send :include, DataMapper::Form::Labels
+DataMapper::Form::Base.send :include, DataMapper::Form::Descriptions
+DataMapper::Form::Base.send :include, DataMapper::Form::Wrappers
 
-form :action => '/login' do
-  textfield :name, :id => 'username'
-  textfield :pass, :id => 'password'
+markup = form :action => '/login' do
+  textfield :name, :label => 'Username', :description => 'Enter your username.'
+  textfield :pass, :label => 'Password', :description => 'Enter your password.'
   submit :value => 'Login'
 end
+
+puts markup
