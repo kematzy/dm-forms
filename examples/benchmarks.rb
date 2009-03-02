903b2740bd42e78c7dd2481e27c1bf3f70c3e539
TJ Holowaychuk <tj@vision-media.ca>
2009-03-02 09:39:38 -0800

    - Altering benchmarks

--- examples/benchmarks.rb
+++ examples/benchmarks.rb
@@ -5,17 +5,20 @@ require 'rubygems'
 require 'rgauge'
 
 include DataMapper::Form::Helpers
+DataMapper::Form::Base.send :include, DataMapper::Form::Labels
+DataMapper::Form::Base.send :include, DataMapper::Form::Descriptions
+DataMapper::Form::Base.send :include, DataMapper::Form::Wrappers
 
 def params
   Hash.new { |h, k| k }
 end
 public :params
 
-benchmark 'Entire forms', :times => 10 do
+benchmark 'Entire forms', :times => 20 do
   report 'Login' do 
     form :action => '/login' do
-      textfield :name, :id => 'username'
-      textfield :pass, :id => 'password'
+      textfield :name, :id => 'username', :label => 'Username'
+      textfield :pass, :id => 'password', :label => 'Password'
       submit :value => 'Login'
     end
   end
