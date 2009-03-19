6b7a436b442b7b9523a5eae35d59bf35e1d447e0
TJ Holowaychuk <tj@vision-media.ca>
2009-03-19 09:38:34 -0700

    Added mixins to benchmarks

--- examples/benchmarks.rb
+++ examples/benchmarks.rb
@@ -5,21 +5,30 @@ require 'rubygems'
 require 'rgauge'
 
 include DataMapper::Form::Helpers
-DataMapper::Form::Base.send :include, DataMapper::Form::Labels
-DataMapper::Form::Base.send :include, DataMapper::Form::Descriptions
-DataMapper::Form::Base.send :include, DataMapper::Form::Wrappers
 
 def params
   Hash.new { |h, k| k }
 end
 public :params
 
-benchmark 'Entire forms', :times => 20 do
+benchmark 'Entire forms', :times => 80 do
   report 'Login' do 
     form :action => '/login' do
-      textfield :name, :id => 'username', :label => 'Username'
-      textfield :pass, :id => 'password', :label => 'Password'
+      textfield :name, :id => 'username'
+      textfield :pass, :id => 'password'
       submit :value => 'Login'
     end
+  end
+  
+  DataMapper::Form::Base.send :include, DataMapper::Form::Labels
+  DataMapper::Form::Base.send :include, DataMapper::Form::Descriptions
+  DataMapper::Form::Base.send :include, DataMapper::Form::Wrappers
+  
+  report 'Login with mixins' do 
+    form :action => '/login' do
+      textfield :name, :id => 'username', :label => 'Username'
+      textfield :pass, :id => 'password', :label => 'Password', :description => 'Enter a valid password.'
+      submit :value => 'Login'
+    end    
   end
 end
